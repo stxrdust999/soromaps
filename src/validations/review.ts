8fb4c73f94b7ad612ebc1a3a344351a96082c1bb
arthur <arthurgoes1411@gmail.com>
2026-08-15 13:13:56 -0300

    🗑️ remove: apaga os órfãos de quando estava planejado implementar o firebase

--- src/validations/review.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const reviewFormSchema = z.object({
-  userId: z.string(),
-  username: z.string(),
-  content: z.string().min(1, "Escreva uma avaliação"),
-  rating: z.number().min(1, "Avalie o local").max(5),
-});
